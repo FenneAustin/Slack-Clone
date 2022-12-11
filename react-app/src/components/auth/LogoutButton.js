74f65407da251389297dcd5d6380b3c7eb1484fd
FenneAustin <austindfenne@gmail.com>
2022-12-10 18:24:44 -0800

    fixed workspace not showing properly

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
 import './LogoutButton.css';
+import { clearWorkspace } from '../../store/ui';
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    await dispatch(clearWorkspace())
   };
 
   return <button onClick={onLogout} className="logout-btn">Logout of slack</button>;
