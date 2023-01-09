39febc5139c42b46dd0fe0e707698a92f47bd05a
FenneAustin <austindfenne@gmail.com>
2023-01-09 14:41:01 -0800

    fixes for models

--- app/models/messages.py
+++ app/models/messages.py
@@ -1,6 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from flask_login import UserMixin
-from datetime import datetime
+import datetime
 
 
 class Message(db.Model, UserMixin):
